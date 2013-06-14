497f61217fb9a6a1e1dfa1317c318917ae36790d
Aaron Gotwalt <gotwalt@seesaw.co>
2013-06-14 12:00:03 -0700

    Crop

--- lib/lapse/client/frames.rb
+++ lib/lapse/client/frames.rb
@@ -13,6 +13,17 @@ module Lapse
       def destroy_frame(clip_id, frame_id)
         boolean_from_response(:delete, "clips/#{clip_id}/frames/#{frame_id}")
       end
+
+      def crop_frame(clip_id, frame_id, x, y, width, height)
+        params = {
+          x: x,
+          y: y,
+          width: width,
+          height: height
+        }
+        boolean_from_response(:post, "clips/#{clip_id}/frames/#{frame_id}/crop", params)
+      end
+
     end
   end
 end
